3e1bb1f170f728c9c318ec35fe950ba2537cde83
Juha <deliciouscookieproduction@gmail.com>
2018-02-12 21:46:57 +0200

    Added game instance

---
+++ Source/PuzzlePlatforms/Private/PPuzzlePlatformsGameInstance.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Public/PPuzzlePlatformsGameInstance.h"
+
+UPPuzzlePlatformsGameInstance::UPPuzzlePlatformsGameInstance()
+{
+	UE_LOG(LogTemp, Warning, TEXT("I am constructo"));
+}
+
+void UPPuzzlePlatformsGameInstance::Init()
+{
+	UE_LOG(LogTemp, Warning, TEXT("I am Int"));
+}
+
+
+
+
---
+++ Source/PuzzlePlatforms/Public/PPuzzlePlatformsGameInstance.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "PPuzzlePlatformsGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEPLATFORMS_API UPPuzzlePlatformsGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+	UPPuzzlePlatformsGameInstance();
+	virtual void Init() override;
+	
+	
+	
+	
+};
